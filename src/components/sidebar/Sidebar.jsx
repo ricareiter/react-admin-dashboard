405c9a07db6009d9257988c09ed850cb8fe4b94a
Ricardo Reiter <ricardoreiterr@gmail.com>
2022-05-04 16:26:16 -0300

    update

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import ContentPasteSearchIcon from "@mui/icons-material/ContentPasteSearch";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import CookieIcon from "@mui/icons-material/Cookie";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Dashboard</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonOutlineOutlinedIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <CookieIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <HealthAndSafetyIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <ContentPasteSearchIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
